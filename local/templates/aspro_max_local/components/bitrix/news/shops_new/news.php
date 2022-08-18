2335c2aea04eb0ec641423cc836440c10c494023
webc <dev@web-c.ru>
2022-08-18 17:40:17 +0300

    фикс карты

--- local/templates/aspro_max_local/components/bitrix/news/shops_new/news.php
+++ local/templates/aspro_max_local/components/bitrix/news/shops_new/news.php
@@ -20,25 +20,18 @@ if($arItems)
 	<div class="contacts-page-top">
 		<div class="contacts maxwidth-theme" itemscope itemtype="http://schema.org/Organization">
 			<div class="row">
+
 				<?$bHasSections = (isset($arAllSections['ALL_SECTIONS']) && $arAllSections['ALL_SECTIONS']);?>
 				<?$bHasChildSections = (isset($arAllSections['CHILD_SECTIONS']) && $arAllSections['CHILD_SECTIONS']);?>
 				<?if($bHasSections):?>
 					<div class="col-lg-6 col-md-12 region-row">
 						<div class="row">
-							<div class="col-lg-6 col-md-4 col-sm-6">
-								<select class="<?=($bHasChildSections ? 'region' : 'city');?>">
-									<option value="0" selected><?=Loc::getMessage('CHOISE_ITEM_CUSTOM')?></option>
-									<?foreach($arAllSections['ALL_SECTIONS'] as $arSection):?>
-										<option value="<?=$arSection['SECTION']['ID'];?>"><?=$arSection['SECTION']['NAME'];?></option>
-									<?endforeach;?>
-								</select>
-							</div>
-							<?if($bHasChildSections):?>
+							<?if($bHasSections):?>
 								<div class="col-lg-6 col-md-4 col-sm-6">
 									<select class="city">
-										<option value="0" selected><?=Loc::getMessage('CHOISE_ITEM', array('#ITEM#' => Loc::getMessage('CITY')))?></option>
-										<?foreach($arAllSections['CHILD_SECTIONS'] as $arSection):?>
-											<option style="display:none;" disabled="disabled" value="<?=$arSection['ID'];?>" data-parent_section="<?=$arSection['IBLOCK_SECTION_ID'];?>"><?=$arSection['NAME'];?></option>
+										<option value="0" selected><?=Loc::getMessage('CHOISE_ITEM_CUSTOM')?></option>
+										<?foreach($arAllSections['ALL_SECTIONS'] as $arSection):?>
+											<option value="<?=$arSection['SECTION']['ID'];?>" data-parent_section="0"><?=$arSection['SECTION']['NAME'];?></option>
 										<?endforeach;?>
 									</select>
 								</div>
