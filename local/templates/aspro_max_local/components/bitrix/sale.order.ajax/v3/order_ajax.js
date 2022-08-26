53fd5f31d2eac23a2425146f8b831fadf2c3930c
Alexander <qzaff32@mail.ru>
2022-08-26 13:59:59 +0300

    поле адрес в корзине

--- local/templates/aspro_max_local/components/bitrix/sale.order.ajax/v3/order_ajax.js
+++ local/templates/aspro_max_local/components/bitrix/sale.order.ajax/v3/order_ajax.js
@@ -3739,12 +3739,12 @@ BX.namespace("BX.Sale.OrderAjaxComponent"), function () {
             }
             if(zip){
                 for (var i in this.deliveryPropsArray){
-                    console.log(this.deliveryPropsArray[i]);
-                    if(this.deliveryPropsArray[i].getId() == 53){
-                        var newzip = this.deliveryPropsArray[i];
-                        delete this.deliveryPropsArray[i];
-                        this.deliveryPropsArray.push(newzip);
-                    }
+                    console.log(this.deliveryPropsArray[i].getId());
+                    // if(this.deliveryPropsArray[i].getId() == 53){
+                    //     var newzip = this.deliveryPropsArray[i];
+                    //     delete this.deliveryPropsArray[i];
+                    //     this.deliveryPropsArray.push(newzip);
+                    // }
                 }
                 this.deliveryPropsArray = this.deliveryPropsArray.filter(function (el) {
                     return (el != null && el != "" || el === 0);
