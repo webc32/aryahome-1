36dbd3b7e60309aad0baeb835230a98b01a0b50a
ivancosovan <72981605+ivancosovan@users.noreply.github.com>
2022-09-02 10:55:37 +0300

    delFromBasketCounter
    Retest delFromBasketCounter

--- local/templates/aspro_max_local/js/main.js
+++ local/templates/aspro_max_local/js/main.js
@@ -2367,12 +2367,12 @@ if (!funcDefined("delFromBasketCounter")) {
               }
             }
             waitLayer(100, function () {
-//               dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
-//               dataLayer.push({
-//                 event: arAsproOptions["COUNTERS"]["GOOGLE_EVENTS"]["REMOVE_BASKET"],
-//                 currency: item.CURRENCY,
-//                 value: parseFloat(item.PRICE),
-//                 ecommerce: ecommerce,
+              dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+              dataLayer.push({
+                event: arAsproOptions["COUNTERS"]["GOOGLE_EVENTS"]["REMOVE_BASKET"],
+                currency: item.CURRENCY,
+                value: parseFloat(item.PRICE),
+                ecommerce: ecommerce,
 //               });
               if (typeof callback == "function") {
                 callback();
