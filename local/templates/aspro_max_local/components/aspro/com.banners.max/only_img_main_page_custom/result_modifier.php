cd4c810ba677057fa359288f87c2aed590d9068b
webc32 <95615650+webc32@users.noreply.github.com>
2022-09-28 09:27:20 +0300

    Add files via upload
    шаблон баннера

---
+++ local/templates/aspro_max_local/components/aspro/com.banners.max/only_img_main_page_custom/result_modifier.php
@@ -0,0 +1,21 @@
+<?
+$arResult = array();
+$iblockIDCustomBanner = 34; 
+$iblockSectionIDCustomBanner = 406; 
+$res = CIBlockElement::GetList(
+    array("ID"=>"DESC"),
+    array("IBLOCK_ID" => $iblockIDCustomBanner, "SECTION_ID" => $iblockSectionIDCustomBanner, "ACTIVE" => "Y"),
+    false,
+    false,
+    array("ID", "IBLOCK_ID", "NAME", "PROPERTY_BG_BANNER_1", "PROPERTY_BG_BANNER_2", "PROPERTY_BG_BANNER_3", "PROPERTY_ACTIVE_BANNER_1", "PROPERTY_ACTIVE_BANNER_2", "PROPERTY_ACTIVE_BANNER_3",
+    "PROPERTY_TITLE_BANNER_1", "PROPERTY_TITLE_BANNER_3", "PROPERTY_TEXT_BTN_BANNER_1", "PROPERTY_TEXT_BTN_BANNER_2", "PROPERTY_TEXT_BTN_BANNER_3", "PROPERTY_LINK_BANNER_1", "PROPERTY_LINK_BANNER_2", "PROPERTY_LINK_BANNER_3",
+    "PROPERTY_DOP_TEXT_BANNER_3", "PROPERTY_COLOR_BG",  "PROPERTY_COLOR_BG_2", "PROPERTY_COLOR_BG_3")
+);
+$newResult = array();
+while($ar_fields = $res->GetNext())
+{
+    $newResult[] = $ar_fields;
+}
+$arResult["ITEMS"] = $newResult;
+?>
+
